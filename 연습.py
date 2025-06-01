f85ce3a7cecf6bfd40faafac7a34cfd7c590ea38
김찬영 <chan@gimchan-yeong-ui-MacBookAir.local>
2025-06-01 17:59:44 +0100

    Remove 연습.py test file

--- 연습.py
+++
@@ -1,9 +0,0 @@
-thisdict={
-    "brand":"Ford",
-    "electric": False,
-    "model":"Mustang",
-    "year":1964,
-    "colors": ["red", "green", "blue"]
-}
-if "model"in thisdict:
-    print("Yes")
